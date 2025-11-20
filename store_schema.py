71715f935d541c81b01ca1f626b1d99fceb31ed9
KittenAttack <guardian-of-devotion@yandex.ru>
2025-11-21 02:51:04 +0300

    Добавил тесты: 1)Размещение заказа 2)Получение информации о заказе по ID 3)Удаление заказа по ID 4)Попытка получить информацию о несуществующем заказе 5)Получение инвентаря магазина
    Добавил фикстуру для создания
    заказа, сделал JSON-схему для
    валидации JSON в ответе в 5
    тесте.

---
+++ store_schema.py
@@ -0,0 +1,16 @@
+STORE_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "approved": {
+            "type": "integer"
+        },
+        "placed": {
+            "type": "integer"
+        },
+        "delivered": {
+            "type": "integer"
+        }
+    },
+    "required": ["approved", "placed", "delivered"],
+    "additionalProperties": False
+}
